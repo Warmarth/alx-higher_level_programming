eefab7a581059137032ea0da199df9945f4ad1ba
Warmarth <godsonibeaka@gmail.com>
2022-09-28 18:35:55 -0700

    Task 13

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 12-roman_to_int.py
 
 
 def roman_to_int(roman_string):
@@ -25,7 +24,7 @@ def roman_to_int(roman_string):
 
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+            num += roman_dict[roman_string[i]] * -1
 
         else:
             num += roman_dict[roman_string[i]]
