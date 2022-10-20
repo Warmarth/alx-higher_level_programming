00514eb5ec11ef407cd4e46b84aafb707d227a82
Warmarth <godsonibeaka@gmail.com>
2022-10-19 18:02:10 -0700

    update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(words = []):
-	words += ["Best school"]
-        return ", ".join(words)
+def magic_string(list_n=[]):
+    list_n += ["BestSchool"]
+    return ", ".join(list_n)
