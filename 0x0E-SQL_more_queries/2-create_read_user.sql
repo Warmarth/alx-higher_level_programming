7b479de050643d0e28ac0c8ec198c090a24f8b2f
Warmarth <godsonibeaka@gmail.com>
2022-11-30 15:56:03 -0800

    read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,8 @@
--- a script that creates the database in server
+-- Creates a database in the server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- select privilege in the database only
-CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';`
+-- Creates a user with only SELECT privileges
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
